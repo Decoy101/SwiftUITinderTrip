445a8067590fd0c0dfef4900546c0fad2af66c0f
Decoy101 <00amangpt00@gmail.com>
2023-12-27 17:56:57 +0530

    Initial Commit

---
+++ SwiftUITinderTrip/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITinderTrip
+//
+//  Created by Simon Ng on 3/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ SwiftUITinderTrip/SwiftUITinderTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITinderTripApp.swift
+//  SwiftUITinderTrip
+//
+//  Created by Simon Ng on 3/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITinderTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
